41489884b24cba17bd163bc416502771fb735dd4
Hector <hectorlucioholguin@gmail.com>
2026-02-24 12:01:07 -0600

    cambio de diseño al menu (puertos del ratchet 8080)

--- main/PW_Test/weather.js
+++ main/PW_Test/weather.js
@@ -1,26 +1,28 @@
 const apikey = "add6fb2d4a56082bd574efe3e676da6c";
-let city = "Nava, MX";
 let units = "metric";
-const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}&lang=es`;
-console.log(api);
-let pageData = document.getElementById("data");
-let img = document.getElementById("img");
-console.log(pageData);
 
+function apiFetching(city, dataId, imgId) {
 
-function apiFetching() {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}&lang=es`;
+
     fetch(api)
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
+
         let temp = data.main.temp;
         let icon = data.weather[0].icon;
-        console.log(data.weather[0].icon);
+
         let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-        pageData.innerHTML = temp;
-        img.src = iconUrl;
-        console.log(iconUrl);
+
+        document.getElementById(dataId).innerHTML = `${temp} °C`;
+        document.getElementById(imgId).src = iconUrl;
     })
+    .catch((error) => {
+        console.log("Error:", error);
+    });
 }
 
-apiFetching();
+apiFetching("Nava, MX", "data1", "img1");
+apiFetching("Monclova, MX", "data2", "img2");
+apiFetching("Piedras Negras, MX", "data3", "img3");
