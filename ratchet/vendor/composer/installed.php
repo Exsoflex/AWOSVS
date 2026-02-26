6759d240696e0f0a757bf29538d735fc82cd06c9
abdielmontesg-glitch <abdielmontesg@gmail.com>
2026-02-26 10:51:06 -0600

    Modulo ciudades actualizado

--- ratchet/vendor/composer/installed.php
+++ ratchet/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6ca31f651a61f7dd9ab4c01c2f3f5579b86dc9ec',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6ca31f651a61f7dd9ab4c01c2f3f5579b86dc9ec',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
